98e4400eba602724fbb38c1ed1f96c8497a8df60
lauriluca99 <67115939+lauriluca99@users.noreply.github.com>
2022-06-16 11:56:18 +0200

    Add files via upload

--- test/TGW3D.jl
+++ test/TGW3D.jl
@@ -1,8 +1,7 @@
-using TGW3D
-using Test
 using LinearAlgebraicRepresentation
 Lar=LinearAlgebraicRepresentation
-
+using TGW3D
+using Test
 
 V = [0.3229956 0.8459234 0.3091824 0.8321101 0.3233606 0.8462884 0.3095474 0.8324751 0.822632 0.8740866 0.8719075 0.923362 0.9865184 1.0379729 1.0357938 1.0872484; 
     0.5770385 0.5632252 0.0548407 0.0410274 0.6046603 0.590847 0.0824624 0.0686491 0.4321656 0.4814411 0.591786 0.6410615 0.368702 0.4179775 0.5283224 0.5775979; 
@@ -10,11 +9,13 @@ V = [0.3229956 0.8459234 0.3091824 0.8321101 0.3233606 0.8462884 0.3095474 0.832
 CV = [[1, 2, 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16]]
 FV = [[1, 2, 3, 4], [5, 6, 7, 8], [1, 2, 5, 6], [3, 4, 7, 8], [1, 3, 5, 7], [2, 4, 6, 8], [9, 10, 11, 12], [13, 14, 15, 16], [9, 10, 13, 14], [11, 12, 15, 16], [9, 11, 13, 15], [10, 12, 14, 16]]
 EV = [[1, 2], [3, 4], [5, 6], [7, 8], [1, 3], [2, 4], [5, 7], [6, 8], [1, 5], [2, 6], [3, 7], [4, 8], [9, 10], [11, 12], [13, 14], [15, 16], [9, 11], [10, 12], [13, 15], [14, 16], [9, 13], [10, 14], [11, 15], [12, 16]]
+mx = Matrix(Lar.boundary_1(EV))
+
 
 cop_EV = Lar.coboundary_0(EV::Lar.Cells)
 cop_FE = Lar.coboundary_1(V, FV::Lar.Cells, EV::Lar.Cells)
 W = convert(Lar.Points, V')
-#TGW3D.spatial_arrangement(W, cop_EV, cop_FE, false)
+TGW3D.spatial_arrangement(W, cop_EV, cop_FE, false)
 
 
 
@@ -29,7 +30,7 @@ W = convert(Lar.Points, V')
 
     err = nothing
     try
-        @inferred TGW3D.merge_vertices(V, EV, FE, 1e-4)
+        @inferred TGW3D.merge_vertices(V, EV, FE, err=1e-4)
     catch err
     end
     @test isa(err, ErrorException) == false
@@ -37,22 +38,14 @@ W = convert(Lar.Points, V')
     err = nothing
     try
         @inferred TGW3D.spatial_arrangement_1(
-            W, cop_EV, cop_FE, multiproc=false)
+            V,copEV,copFE, multiproc=false)
     catch err
     end
     @test isa(err, ErrorException) == false
 
     err = nothing
     try
-        @inferred TGW3D.minimal_3cycles(W, cop_EV, cop_FE)
-
-    catch err
-    end
-    @test isa(err, ErrorException) == false
-
-    err = nothing
-    try
-        @inferred TGW3D.spatial_arrangement_2(W, cop_EV, cop_FE)
+        @inferred TGW3D.minimal_3cycles(V, EV, FE)
 
     catch err
     end
@@ -61,9 +54,41 @@ W = convert(Lar.Points, V')
     err = nothing
     try
         @inferred TGW3D.spatial_arrangement(
-            W, cop_EV, cop_FE, multiproc=false)
+            V, copEV, copFE, multiproc=false)
     catch err
     end
     @test isa(err, ErrorException) == false
 
 end
+
+
+
+
+# questa funzione ci mostra come ogni colonna della matrice sparsa di EV contiene sempre un 1 e un -1
+# infatti si nota come, utilizzando random, viene presa ogni volta una colonna a caso e il contatore 
+# riporterà sempre il valore 2
+function doubleOne()
+  mx = Matrix(Lar.boundary_1(EV))
+  count = 0
+  j = rand(1:24)
+  for i in 1:16
+      if mx[i,j]==1 || mx[i,j]==-1
+         count+=1
+        end
+    
+    end
+    return count
+end
+
+@testset "test matrice sparsa" begin
+    @test doubleOne() == 2
+
+end
+
+# test per verificare le incidenze della matrice sparsa
+@testset "test incidenze" begin
+    @test mx[1,1]== -1
+    @test mx[2,1]== 1
+
+end
+
